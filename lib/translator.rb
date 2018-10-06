0c6d1007d3ae93d006c0a3595c99462640f48b9c
Solee Lee <carolinesoleelee@gmail.com>
2018-10-06 05:34:49 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,5 @@
 require "yaml"
 
 def load_library(file_path)
-hash =
-{
-  "get_meaning" => "Japanese emoticons",
-  "get_emoticon" => "English emoticons"
-}
+hash = {"get_meaning" => {}, "get_emoticon" => {} }
 end
